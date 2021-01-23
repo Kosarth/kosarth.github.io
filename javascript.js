355676a3f4c3097ac69afb769f6106e86c912b0d
Arthur Santos <arthur.santos@byside.com>
2021-01-23 18:40:48 +0000

    Improved index.html

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 var quotes = [
         '"When you talk, you are only repeating what you already know. But when you listen, you may learn something new." ~ The Dalai Lama',
-        '"Success is how high you bounce when you hit the bottom." ~ General George S. Patton, U.S. Army',
-        '"Courage is resistance to fear, mastery of fear - not absence of fear." ~ Mark Twain',
+        '"Success is how high you bounce when you hit the bottom." ~ General George S. Patton',
         '“Inflation takes from the ignorant and gives to the well informed.” ~ Venita VanCaspel',
         '"I fear not the man who has practiced 10,000 kicks once, but the man who has practiced one kick 10,000 times." ~ Bruce Lee',
         '“Most people never pick up the phone. Most people never call and ask. And that’s what separates sometimes the people who do things from those who just dream about them. You gotta act. You gotta be willing to fail. You gotta be willing to crash a burn. With people on the phone or starting a company, if you’re afraid you’ll fail, you won’t get very far.” ~ Steve Jobs',
